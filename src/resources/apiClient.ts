91209c375ed55d35708a0902dd3dbfa18276b104
Idriss Mahjoubi <mahjoubi.idris@gmail.com>
2025-04-16 14:23:07 +0100

    Initial commit

---
+++ src/resources/apiClient.ts
@@ -0,0 +1,80 @@
+export interface ApiClientOptions {
+  apiKey?: string;
+  baseUrl?: string;
+}
+export class ApiClient {
+  private readonly baseUrl: string;
+  private readonly apiKey: string;
+
+  constructor({
+    apiKey,
+    baseUrl = "https://rest.eu-west-1.nextapp.co/v1",
+  }: ApiClientOptions) {
+    if (!apiKey || apiKey.length === 0) {
+      throw new Error("API key is required");
+    }
+    this.baseUrl = baseUrl;
+    this.apiKey = apiKey;
+  }
+
+  async getHighlights<T = unknown>(
+    params: {
+      from?: number;
+      limit?: number;
+      playlistsLabels?: string[];
+      similarityCutoff?: number;
+      sort?: string;
+      sortDirection?: "asc" | "desc";
+      search_term?: string;
+    } = {}
+  ): Promise<T[]> {
+    return this.fetchApi<T>("search-highlights", params);
+  }
+
+  async getClusters<T = unknown>(
+    params: {
+      from?: number;
+      limit?: number;
+      similarityCutoff?: number;
+      sort?: string;
+      search_term?: string;
+    } = {}
+  ): Promise<T[]> {
+    return this.fetchApi<T>("search-playlists", params);
+  }
+
+  /** Internal helper to DRY up common fetch logic */
+  private async fetchApi<T>(
+    endpoint: string,
+    params: Record<string, any> = {}
+  ): Promise<T[]> {
+    const queryString = new URLSearchParams(params).toString();
+    const url = `${this.baseUrl}/${endpoint}?${queryString}`;
+    process.stderr.write('url');
+    process.stderr.write(url);
+    process.stderr.write('apiKey:')
+    process.stderr.write(this.apiKey);
+    const options: RequestInit = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: this.apiKey,
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(
+          `API error (${response.status}): ${response.statusText}`
+        );
+      }
+
+      return (await response.json()) as T[];
+    } catch (error) {
+      console.error("Fetch error:", error);
+      throw error;
+    }
+  }
+}
---
+++ src/utils/build-clusters-response.ts
@@ -0,0 +1,59 @@
+import { MinimalPlaylist } from "../types";
+import { isExisting } from "./is-existing";
+
+function toNrLinkedHighlightsText(playlist: MinimalPlaylist) {
+  return `Users raised this point ${playlist.nr_linked_highlights} times.`;
+}
+
+function toLabelsText(playlist: MinimalPlaylist) {
+  return (playlist.labels ?? [])
+    .map((label) => `#${label.replace(/ /g, "-")}`)
+    .join(" ");
+}
+
+function getTextsFromSinglePlaylist(playlist: MinimalPlaylist) {
+  // If there is only one playlist: Use also the playlist title itself because it provides
+  // more "depth of information" (and we have the space for it). Additionally, we use the
+  // description because it provides more context
+  return [
+    `[${playlist.id}]:`,
+    playlist.description,
+    playlist.title,
+    toNrLinkedHighlightsText(playlist),
+    toLabelsText(playlist),
+  ];
+}
+
+function getTextsFromMultiplePlaylists(playlists: MinimalPlaylist[]) {
+  // Use descriptions if there are many playlists because they provide a compact
+  // summary, i.e. we can cover more "breath of information"
+  return playlists.map((playlist) => {
+    const labelsText = toLabelsText(playlist);
+    const nrHighlights = toNrLinkedHighlightsText(playlist);
+    return `[${playlist.id}]: ${playlist.description} ${nrHighlights} ${labelsText}`;
+  });
+}
+
+export function buildPlaylistsForPrompt(playlists: MinimalPlaylist[]) {
+  process.stderr.write(
+    `Creating prompts based on ${playlists.length} playlists`
+  );
+
+  const hasPlaylists = playlists.length > 0;
+
+  if (!hasPlaylists) {
+    return [];
+  }
+
+  const isSinglePlaylist = playlists.length === 1;
+  const texts = isSinglePlaylist
+    ? getTextsFromSinglePlaylist(playlists[0])
+    : getTextsFromMultiplePlaylists(playlists);
+
+  const cleanedTexts = texts.filter(isExisting).map((text) =>
+    // Remove duplicate white spaces
+    text.replace(/\s{2,}/g, " ").trim()
+  );
+
+  return cleanedTexts;
+}
